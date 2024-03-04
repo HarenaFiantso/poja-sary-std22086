cf2d4c426ad19d78aec222e03943e29451e254d4
HarenaFiantso <fiantsorav@gmail.com>
2024-03-04 11:23:40 +0300

    feat: add GET /black-and-white/{id}

--- src/main/java/school/hei/sary/endpoint/rest/controller/ImageController.java
+++ src/main/java/school/hei/sary/endpoint/rest/controller/ImageController.java
@@ -1,10 +1,7 @@
 package school.hei.sary.endpoint.rest.controller;
 
 import lombok.AllArgsConstructor;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import school.hei.sary.dto.ImageDTO;
 import school.hei.sary.service.ImageService;
 
@@ -18,6 +15,15 @@ public class ImageController {
       @PathVariable(name = "id") String id, @RequestBody(required = false) byte[] file) {
     try {
       return imageService.uploadAndConvertImageToBlackAndWhite(id, file);
+    } catch (Exception e) {
+      return null;
+    }
+  }
+
+  @GetMapping(value = "/black-and-white/{id}")
+  public ImageDTO getImageURL(@PathVariable(name = "id") String id) {
+    try {
+      return imageService.getPicturesURL(id);
     } catch (Exception e) {
       return null;
     }
